d9891a34c6e2a5a9c917bec6fb898f0b9b3a86d4
tracer-ics2023 <tracer@njuics.org>
2024-03-27 15:37:07 +0800

    >  compile NEMU 221220000 张三 Linux liujianxia-System-Product-Name 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:37:07 up 5 days, 23:42,  3 users,  load average: 0.01, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -24,7 +24,7 @@ enum {
   TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
-
+  TK_INT,
 };
 
 static struct rule {
@@ -39,6 +39,9 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+
+  {"\\d+", TK_INT},
+
 };
 
 #define NR_REGEX ARRLEN(rules)
