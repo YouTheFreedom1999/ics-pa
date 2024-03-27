f8bc620c7ba32807284677ca9332458e0ec92d41
tracer-ics2023 <tracer@njuics.org>
2024-03-27 15:06:26 +0800

    >  compile NEMU 221220000 张三 Linux liujianxia-System-Product-Name 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:26 up 5 days, 23:11,  3 users,  load average: 0.01, 0.02, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for(int i = 0; i < 32; i++){
-    printf("%s: 0x%lx\n", regs[i], gpr(i));
+    printf("%s: 0x%016lx\n", regs[i], gpr(i));
   }
 }
 
