7997cd88955e0b8991fe8a2cb4061d0bea37087f
tracer-ics2023 <tracer@njuics.org>
2024-03-27 18:11:11 +0800

    >  run NEMU 221220000 张三 Linux liujianxia-System-Product-Name 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:11:11 up 6 days,  2:16,  6 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -101,6 +101,10 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
+
+        if(rules[i].token_type == TK_NOTYPE)
+          continue;
+
         assert(nr_token<32);
         tokens[nr_token++].type  = rules[i].token_type;
         
@@ -123,14 +127,95 @@ static bool make_token(char *e) {
 }
 
 
+int check_parentheses(int p , int q) {
+  if(q-p<2)
+    return -1;
+  int match = 0;
+  for(int i = p;i<q+1;i++){
+    if(tokens[i].type == '('){
+      match++;
+    }
+    if(tokens[i].type == ')'){
+      match--;
+    }
+  }
+
+  if(tokens[p].type == '(' && tokens[q].type == ')')
+    return match != 0 ? -2 : 0;
+  else
+    return -3;
+}
+
+int find_main_op(int p , int q , char *op_type){
+  int matching = 0;
+  *op_type = 'u';
+  int index = p;
+  for(int i = p;i<q+1;i++){
+    if(tokens[i].type == '(' )
+      matching++;
+    
+    if(tokens[i].type == ')')
+      matching--;
+    
+    if(matching==0 && i != q){
+      if(tokens[i].type == '+' || tokens[i].type == '-' || *op_type == 'u'){
+        *op_type = tokens[i].type;
+        index = i;
+      }
+    }
+  }
+  if(*op_type == 'u'){
+    assert(0);
+  }else{
+    return index;
+  }
+}
+
+uint32_t eval(int p , int q) {
+  if(p>q){
+    assert(0);
+  }else if(p == q){
+    return atoi(tokens[p].str);
+  }else if(check_parentheses(p,q) == 0){
+    eval(p+1,q-1);
+  }else{
+    char op_type ;
+    int op = find_main_op(p,q , &op_type);
+    uint32_t val1 = eval(p,op-1);
+    uint32_t val2 = eval(op+1,q);
+    switch(op_type)
+      {
+        case '+':
+          return val1 + val2;
+        case '-':
+          return val1 - val2;
+        case '*':
+          return val1 * val2;
+        case '/':
+          return val1 / val2;
+        default:
+          assert(0);
+      }
+  }
+  return 0;
+}
+
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
 
-  /* TODO: Insert codes to evaluate the expression. */
-  // TODO();
+  for(int i = 0;i<nr_token;i++){
+    if(tokens[i].type == TK_INT) 
+      printf("%d" , atoi(tokens[i].str));
+    else
+      printf("%c" , tokens[i].type);
+  }
 
-  return 0;
+  int result =  eval(0 , nr_token-1);
+  printf("%s result is %d\n" , e , result);
+
+  return result;
+
 }
